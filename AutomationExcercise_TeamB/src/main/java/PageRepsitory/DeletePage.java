3eb961a1e1ca9cb9716338be1409ae6f8fb1994b
megha <megha@Meghana>
2025-05-06 16:23:47 +0530

    commit by Meghana

--- AutomationExcercise_TeamB/src/main/java/PageRepsitory/DeletePage.java
+++ AutomationExcercise_TeamB/src/main/java/PageRepsitory/DeletePage.java
@@ -1,35 +1,29 @@
 package PageRepsitory;
-	import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.support.FindBy;
-	import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-	public class DeletePage {
-		
-		//Declaration
-		@FindBy(xpath = "//a[text()='Continue']")
-		private WebElement conDelete;
-		
-		//Initialization
-		public DeletePage(WebDriver driver) {
-			PageFactory.initElements(driver, this);
-		}
-		
-		//Utilization
-		public WebElement getConDelete() {
-			return conDelete;
-		}
+public class DeletePage {
 
-		public WebElement getdeletebutton() {
-			// TODO Auto-generated method stub
-			return getdeletebutton();
-		}
+	// Declaration
+	@FindBy(xpath = "//a[text()='Continue']")
+	private WebElement conDelete;
 
-		public WebElement getdeletevisible() {
-			return deletevisible;
-		}
-		
+	// Initialization
+	public DeletePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
 	}
 
+	// Utilization
+	public WebElement getConDelete() {
+		return conDelete;
+	}
 
+	public WebElement getdeletebutton() {
+		// TODO Auto-generated method stub
+		return getdeletebutton();
+	}
+
+}
---
+++ AutomationExcercise_TeamB/src/test/java/product_Test/TestCase_28.java
@@ -0,0 +1,10 @@
+package product_Test;
+
+import org.testng.annotations.Test;
+
+public class TestCase_28 {
+	@Test
+	public void Meghana() {
+		System.out.println("Learning GitHub");
+	}
+}
